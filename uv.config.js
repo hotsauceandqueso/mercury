01a570e4bfd35667e9050dfb731a620681e2d562
hotsauce <briankshaw1215@proton.me>
2026-01-07 18:10:20 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://tomp.app/bare/', // Using a public bare server
+    bare: 'https://tomp.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
+    config: '/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
